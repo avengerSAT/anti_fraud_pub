3bd45cc9520348c585b6d3ab9262cfa453f98652
IBorushev <winter23rus@gmail.com>
2019-09-23 13:07:58 +0300

    connection-fixed

--- check/con.py
+++ check/con.py
@@ -2,11 +2,12 @@ import os
 
 from dotenv import load_dotenv
 from pathlib import Path
-env_path = Path('./check/') / '.env'
+env_path = Path('antifraud/') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 class Con_vert():
-        host= os.getenv('host')
-        port= os.getenv('port')
-        user= os.getenv('user')
-        password= os.getenv('password')
+        host= os.getenv('host_vertica')
+        port= os.getenv('port_vertica')
+        user= os.getenv('user_vertica')
+        password= os.getenv('password_vertica')
+        
