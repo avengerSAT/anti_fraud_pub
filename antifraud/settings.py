e5f8ae84767c603a6ddd366de65fe51544c2ece2
avengerSAT <avenger.sat@gmail.com>
2019-09-27 16:56:49 +0300

    dash

--- antifraud/settings.py
+++ antifraud/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'antifraud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-  'default': {
-      'ENGINE': 'django.db.backends.sqlite3',
-      'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-  }
-}
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+#   }
+#}
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.mysql',
-#          'NAME': os.getenv('name'),
-#          'USER': os.getenv('user'),
-#          'PASSWORD': os.getenv('password'),
-#          'HOST': os.getenv('host'),
-#          'PORT': os.getenv('port')
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': os.getenv('name'),
+         'USER': os.getenv('user'),
+         'PASSWORD': os.getenv('password'),
+         'HOST': os.getenv('host'),
+         'PORT': os.getenv('port')
+    }
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
